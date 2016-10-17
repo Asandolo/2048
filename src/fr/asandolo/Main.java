bc0cca18057954362140530b59f88b7048b2a0b4
Asandolo <asandolo3@gmail.com>
2016-10-17 14:55:23 +0200

    Git ok !

--- src/fr/asandolo/Main.java
+++ src/fr/asandolo/Main.java
@@ -21,8 +21,6 @@ public class Main {
        m.setval(2, 0, 0);
        System.out.println(" ");
        m.affiche();
-        System.out.println("test");
-        System.out.println("yolod");
     }
     
 }
