e7c37672e8b9725b73c12fc58edd85c2af6e1bdf
Asandolo <asandolo3@gmail.com>
2016-10-19 13:14:34 +0200

    njfnkkfj

--- src/fr/asandolo/Main.java
+++ src/fr/asandolo/Main.java
@@ -18,48 +18,51 @@ public class Main {
      */
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
+       boolean fq = false;
        char chx = 'A';
        Matrice m = new Matrice(4);
        m.init();
-       m.affiche();
-        System.out.println("");
-       m.generate();
-       m.generate();
-       m.generate();       
-       m.generate();
        m.generate();
        m.affiche();
        
-       do{
-          try{
-             System.out.println("Choisisez : H = Haut, B = Bas, G = Gauche, D = Droite");
-              System.out.print(">");
-             chx = sc.nextLine().charAt(0);
-          }catch(Exception e){
-              System.out.println("Erreur :"+e);
-          }
-          
-          
-       }while(chx != 'D' && chx != 'G' && chx != 'H' && chx != 'B');
-       
-        switch(chx){
-            case 'H':
-                m.haut();
-                break;
-            case 'B':
-                m.bas();
-                break;
-            case 'G':
-                m.gauche();
-            case 'D':
-                m.droite();
-                break;
-            default:
-                System.out.println("Erreur");
-                break;
-                
-        }
-               
+       while(fq == false){
+
+            do{
+               try{
+                  System.out.println("Choisisez : H = Haut, B = Bas, G = Gauche, D = Droite");
+                   System.out.print(">");
+                  chx = sc.nextLine().charAt(0);
+               }catch(Exception e){
+                   System.out.println("Erreur :"+e);
+               }
+
+
+            }while(chx != 'D' && chx != 'G' && chx != 'H' && chx != 'B' && chx != 'f');
+
+             switch(chx){
+                 case 'H':
+                     m.haut();
+                     m.generate();
+                     m.affiche();
+                     break;
+                 case 'B':
+                     m.bas();
+                     break;
+                 case 'G':
+                     m.gauche();
+                 case 'D':
+                     m.droite();
+                     break;
+                 case 'f':
+                     fq = true;
+                     break;                     
+                 default:
+                     System.out.println("Erreur");
+                     break;
+
+             }
+
+         }
     }
     
 }
