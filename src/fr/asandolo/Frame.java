c459e86c2ef48a56e204856f0087f1082e37ba9a
Asandolo <asandolo3@gmail.com>
2016-11-06 21:25:45 +0100

    Commit des familles

--- src/fr/asandolo/Frame.java
+++ src/fr/asandolo/Frame.java
@@ -12,8 +12,13 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 
@@ -23,15 +28,27 @@ import javax.swing.JPanel;
  */
 public class Frame extends JFrame {
     
-    
+    private Matrice matrice;
     private JPanel p,btns;
     private Content c;
     private JButton btn_triche,btn_reset,btn_quitte;
     
-    public Frame(){
+    public Frame(Matrice m){
+        this.matrice = m;
+        
+        BtnListener b = new BtnListener();
+        
         this.btn_triche = new JButton("Trichez");
+        this.btn_triche.setActionCommand("triche");
+        this.btn_triche.addActionListener(b);
+        
         this.btn_reset = new JButton("Reset");
+        this.btn_reset.setActionCommand("reset");
+        this.btn_reset.addActionListener(b);
+        
         this.btn_quitte = new JButton("Quitter");
+        this.btn_quitte.setActionCommand("quitte");
+        this.btn_quitte.addActionListener(b);
         
         this.btns = new JPanel();
         this.btns.add(this.btn_triche);
@@ -50,25 +67,106 @@ public class Frame extends JFrame {
         this.setMinimumSize(new Dimension(400,400));
         this.setPreferredSize(new Dimension(600,600));
         this.setResizable(true);
+        this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(this.p);
         this.setVisible(true);
+        
+        
+        
+        this.c.addKeyListener(new KeyListener() {
+            @Override public void keyTyped(KeyEvent e) {}
+            @Override public void keyPressed(KeyEvent e) {}
+            @Override
+            public void keyReleased(KeyEvent e) {
+                switch(e.getKeyCode()){
+                    case KeyEvent.VK_UP:
+                        matrice.haut();
+                        matrice.generate();
+                        break;
+                    
+                    case KeyEvent.VK_DOWN:
+                        matrice.bas();
+                        matrice.generate();
+                        break;
+                        
+                    case KeyEvent.VK_LEFT:
+                        matrice.gauche();
+                        matrice.generate();
+                        break;
+                        
+                    case KeyEvent.VK_RIGHT:
+                        matrice.droite();
+                        matrice.generate();
+                        break;
+                        
+                }
+                matrice.affiche();
+                c.repaint();
+            }
+        });
+        
+        
+        this.c.requestFocus();
     }
     
-    public void setMatrix(int[][] mat){
-        this.c.setMatrix(mat);
+    private class BtnListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try{
+                switch(e.getActionCommand()){
+                    case "triche":
+                        int line= -1, col = -1;
+                        while(line < 0 || line > matrice.getDim()){
+                            try{
+                                String l = JOptionPane.showInputDialog(c, "Entrez la ligne :", "Triche 2048", JOptionPane.QUESTION_MESSAGE);
+                                line = Integer.parseInt(l);
+                            }catch(Exception ex){}
+                        }
+                        while(col < 0 || col > matrice.getDim()){
+                            try{
+                                String l = JOptionPane.showInputDialog(c, "Entrez la colone :", "Triche 2048", JOptionPane.QUESTION_MESSAGE);
+                                col = Integer.parseInt(l);
+                            }catch(Exception ex){}
+                        }
+                        matrice.setval(0, line-1, col-1);
+                        c.repaint();
+                        
+                        break;
+                        
+                    case "quitte":
+                        if(JOptionPane.showConfirmDialog(c,"Etes-vous certain de vouloir quitter?",
+                                "Quitter 2048?", JOptionPane.YES_NO_OPTION) == 0)
+                        System.exit(0);
+                        break;
+                        
+                    case "reset":
+                        matrice.init();
+                        matrice.generate();
+                        matrice.generate();
+                        c.repaint();
+                        break;
+                    default:
+                        throw new Exception("BTN NON IMPLEMENTE");
+                }
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        
     }
+    
     
     private class Content extends JPanel {
-        int [][] nbs = {{}};
-        
-        public void setMatrix(int[][] mat){
-            this.nbs = mat;
-            repaint();
+        public Content(){
+            this.setFocusable(true);
         }
         
         @Override
         public void paintComponent(Graphics gg){
+            int[][] nbs = matrice.getMatrix();
+            
             Graphics2D g = (Graphics2D)gg;
             
             g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
